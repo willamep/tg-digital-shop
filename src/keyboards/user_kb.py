97068ea99482b3a6e1e0416aab82c10f5bfceb20
willamep <saifer.20060702@gmail.com>
2026-01-06 20:18:16 +0700

    Add keyboard for items catalog

--- src/keyboards/user_kb.py
+++ src/keyboards/user_kb.py
@@ -1,8 +1,8 @@
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-from aiogram.types import InlineKeyboardMarkup
-from aiogram.filters.callback_data import CallbackData
+from aiogram.utils.keyboard         import InlineKeyboardBuilder
+from aiogram.types                  import InlineKeyboardMarkup
+from aiogram.filters.callback_data  import CallbackData
 
-from database.db import get_categories
+from database.db import get_categories, get_items
 
 class MainMenuCB(CallbackData, prefix="main"):
     chapter: str
@@ -10,8 +10,8 @@ class MainMenuCB(CallbackData, prefix="main"):
 class CatalogCB(CallbackData, prefix="cat"):
     cat_id: int
 
-# class ItemCB(CallbackData, prefix="item"):
-#     item_id: int
+class ItemCB(CallbackData, prefix="item"):
+    item_id: int
 
 # class BuyCB(CallbackData, prefix="buy"):
 #     chapter: int
@@ -25,15 +25,15 @@ class CatalogCB(CallbackData, prefix="cat"):
 def main_menu_kb() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     builder.button(text=f"About Shop", callback_data=MainMenuCB(chapter="About Shop"))
-    builder.button(text=f"My Orders", callback_data=MainMenuCB(chapter="My Orders"))
-    builder.button(text=f"Catalog", callback_data=MainMenuCB(chapter="Catalog"))
+    builder.button(text=f"My Orders",  callback_data=MainMenuCB(chapter="My Orders"))
+    builder.button(text=f"Catalog",    callback_data=MainMenuCB(chapter="Catalog"))
     builder.adjust(2, 1)
     return builder.as_markup()
 
 def about_shop_kb() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     builder.button(text=f"My Orders", callback_data=MainMenuCB(chapter="My Orders"))
-    builder.button(text=f"Catalog", callback_data=MainMenuCB(chapter="Catalog"))
+    builder.button(text=f"Catalog",   callback_data=MainMenuCB(chapter="Catalog"))
     builder.adjust(2)
     return builder.as_markup()
     
@@ -43,4 +43,11 @@ def catalog_kb() -> InlineKeyboardMarkup:
     for cat in get_categories():
         builder.button(text=cat[1], callback_data=CatalogCB(cat_id=cat[0]))
     builder.adjust(2)
+    return builder.as_markup()
+
+def items_kb(cat_id) -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+    for item in get_items(cat_id):
+        builder.button(text=f"{item[1]} – {item[2]}", callback_data=ItemCB(item_id=item[0]))
+    builder.adjust(1)
     return builder.as_markup()
