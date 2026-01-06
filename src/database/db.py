29d8939754807c062b3dbad7f316de15d03d7e7a
willamep <saifer.20060702@gmail.com>
2026-01-06 20:15:57 +0700

    Add func pars items

--- src/database/db.py
+++ src/database/db.py
@@ -7,3 +7,11 @@ def get_categories():
     rows = cursor.fetchall()
     conn.close()
     return rows
+
+def get_items(cat_id):
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, item_name, price FROM items WHERE cat_id = ?", (cat_id,))
+    rows = cursor.fetchall()
+    conn.close()
+    return rows
