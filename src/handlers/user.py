bf44b65d888a3f6aeea80ee61418fc5981e438b7
willamep <saifer.20060702@gmail.com>
2026-01-06 10:51:09 +0700

    Refactor(handler): add handler for InlineButtons of main_menu

--- src/handlers/user.py
+++ src/handlers/user.py
@@ -4,7 +4,7 @@ from aiogram.filters import Command, CommandStart
 from aiogram.types import Message, CallbackQuery
 
 # Imports from project files
-from keyboards import main_menu # , catalog_kb
+from keyboards import MainMenuCB, main_menu # , catalog_kb
 # from services.product import get_products
 # from database.db import add_order
 
@@ -15,11 +15,35 @@ router = Router()
 @router.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     await message.answer(
-        f"Hello, {html.bold(message.from_user.full_name)}!\n"
-        f"In this bot You can buy goods from a universe far, far away.",
+        f"{start_text(message.from_user.full_name)}",
         reply_markup=main_menu()
     )
 
 @router.message()
 async def unknown_command(message: Message) -> None:
-    await message.answer("What language is this? I don't understand.")
+    await message.answer(
+        "What language is this? I don't understand. Let's speak at basic.\n\n"
+        f"{start_text(message.from_user.full_name)}",
+        reply_markup=main_menu()
+    ) 
+
+@router.callback_query(MainMenuCB.filter(F.chapter == "Catalog"))
+async def catalog(query: CallbackQuery, callback_data: MainMenuCB):
+    await query.answer()
+    await query.message.edit_text("Catalog")
+
+@router.callback_query(MainMenuCB.filter(F.chapter == "My Orders"))
+async def catalog(query: CallbackQuery, callback_data: MainMenuCB):
+    await query.answer()
+    await query.message.edit_text("Info about orders")
+
+@router.callback_query(MainMenuCB.filter(F.chapter == "About Shop"))
+async def catalog(query: CallbackQuery, callback_data: MainMenuCB):
+    await query.answer()
+    await query.message.edit_text("Info about shop")
+
+def start_text(full_name: str) -> str:
+    return (
+        f"Hello, {html.bold(full_name)}!\n"
+        "In this bot you can buy goods from a universe far, far away."
+    )
